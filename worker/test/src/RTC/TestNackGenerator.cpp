b1f991a7e2d8277368ce6c8e63590b998d21e16c
penguinol <penguinol@163.com>
2022-05-26 21:17:37 +0800

    Update TestNackGenerator.cpp

--- worker/test/src/RTC/TestNackGenerator.cpp
+++ worker/test/src/RTC/TestNackGenerator.cpp
@@ -19,7 +19,8 @@ struct TestNackGeneratorInput
 	  uint16_t firstNacked,
 	  size_t numNacked,
 	  bool keyFrameRequired = false,
-	  size_t nackListSize   = 0)
+	  size_t nackListSize   = 0,
+	  size_t delayMs = 0)
 	  : seq(seq), isKeyFrame(isKeyFrame), firstNacked(firstNacked), numNacked(numNacked),
 	    keyFrameRequired(keyFrameRequired), nackListSize(nackListSize), delayMs(delayMs)
 	{
